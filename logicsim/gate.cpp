68a5f400a126302edb3796e8a3bcfb5b5e026138
Ney <176237631+NeyAtUSC@users.noreply.github.com>
2025-03-11 22:20:33 -0700

    10

--- logicsim/gate.cpp
+++ logicsim/gate.cpp
@@ -88,3 +88,37 @@ Event* Or2Gate::update(uint64_t current_time)
 	}
   return e;
 }
+
+NotGate::NotGate(Wire* in, Wire* out) : Gate(1, out)
+{
+	wireInput(0, in);
+}
+
+Event* NotGate::update(uint64_t current_time)
+{
+	char in_state = m_inputs[0]->getState();
+	char state = 'X';
+
+	switch (in_state) {
+		case '1':
+			state = '0';
+			break;
+		case '0':
+			state = '1';
+			break;
+		case 'X':
+			state = 'X';
+			break;
+		default:
+			throw "How";
+			break;
+	}
+
+	Event* e = nullptr;
+	if (state != m_current_state) {
+		m_current_state = state;
+		uint64_t next = current_time + m_delay;
+		e = new Event { next, m_output, state };
+	}
+	return e;
+}
